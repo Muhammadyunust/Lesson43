c5a057cba89847c900d89f7e13b0a4caf817b292
Muhammadyunus <uzbekstudent1997@gmail.com>
2020-02-06 18:52:20 +0500

    Initial Commit

---
+++ Lesson43/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson43
+//
+//  Created by Mukhammadyunus on 2/6/20.
+//  Copyright © 2020 Mukhammadyunus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
